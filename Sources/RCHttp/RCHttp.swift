413fd5a596afa02b121bdc9cffb1bdf1c7e61c70
Cristian Baluta <cristi.baluta@gmail.com>
2021-01-26 22:38:17 +0200

    Add headers

--- Sources/RCHttp/RCHttp.swift
+++ Sources/RCHttp/RCHttp.swift
@@ -16,6 +16,8 @@ public struct RCHttpError: LocalizedError {
 
 public class RCHttp {
 	
+    public static var loggingEnabled = true
+    
     public var baseURL: URL! = nil
 	private var task: URLSessionTask?
     private var base64LoginData: String?
@@ -39,55 +41,97 @@ public class RCHttp {
         }
     }
     
+    public func setHeader () {
+        
+    }
+    
     /// Do a GET request
-    public func get (at path: String, success: @escaping (HTTPURLResponse, Data) -> Void, failure: @escaping (Error) -> Void) {
+    public func get (at path: String,
+                     headers: [String: String]? = nil,
+                     success: @escaping (HTTPURLResponse, Data) -> Void,
+                     failure: @escaping (Error) -> Void) {
         
         let fullPath = baseURL.appendingPathComponent(path).absoluteString.removingPercentEncoding!
         let url = URL(string: fullPath)!
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
-        print("RCHttp: GET from \(url) -> \(request)")
         
         // Authenticate
         request = authenticate(request)
         
+        // Set extra headers
+        for (key, value) in headers ?? [:] {
+            request.setValue(value, forHTTPHeaderField: key)
+        }
+        
+        if RCHttp.loggingEnabled {
+            print("RCHttp: GET from \(url) -> \(request)")
+        }
+        
         task = URLSession.shared.dataTask(with: request) { data, response, error in
-            print("RCHttp: GET from \(url) response: \(String(describing: response))")
+            if RCHttp.loggingEnabled {
+                print("RCHttp: GET from \(url) response: \(String(describing: response))")
+            }
             guard let response = response as? HTTPURLResponse, let data = data, error == nil else {
-                print("RCHttp: GET from \(url) -> \(error!)")
+                if RCHttp.loggingEnabled {
+                    print("RCHttp: GET from \(url) -> \(error!)")
+                }
                 failure(error!)
                 return
             }
-            print("RCHttp: GET from \(url) data: \(String(data: data, encoding: .utf8) ?? "")")
+            if RCHttp.loggingEnabled {
+                print("RCHttp: GET from \(url) data: \(String(data: data, encoding: .utf8) ?? "")")
+            }
             success(response, data)
         }
         task!.resume()
     }
 	
 	/// Do  a post request
-    public func post (at path: String, parameters: [String: Any], success: @escaping (HTTPURLResponse, Data) -> Void, failure: @escaping (Error) -> Void) {
+    public func post (at path: String,
+                      parameters: [String: Any]? = nil,
+                      headers: [String: String]? = nil,
+                      success: @escaping (HTTPURLResponse, Data) -> Void,
+                      failure: @escaping (Error) -> Void) {
         
         let fullPath = baseURL.appendingPathComponent(path).absoluteString.removingPercentEncoding!
         let url = URL(string: fullPath)!
-        let jsonData = try! JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
 
-		var request = URLRequest(url: url)
-		request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-		request.httpBody = jsonData
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
         request = authenticate(request)
-        print("RCHttp: POST to \(url) -> \(request)")
+        
+        if let parameters = parameters {
+            let jsonData = try! JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+            request.httpBody = jsonData
+        }
+        
+        // Set extra headers
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        for (key, value) in headers ?? [:] {
+            request.setValue(value, forHTTPHeaderField: key)
+        }
+        
+        if RCHttp.loggingEnabled {
+            print("RCHttp: POST to \(url) -> \(request)")
+        }
 		
 		let session = URLSession(configuration: URLSessionConfiguration.ephemeral)
 		task = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) in
 			
-            print("RCHttp: POST to \(url) response: \(String(describing: response))")
+            if RCHttp.loggingEnabled {
+                print("RCHttp: POST to \(url) response: \(String(describing: response))")
+            }
             guard let response = response as? HTTPURLResponse, let data = data, error == nil else {
-                print("RCHttp: POST to \(url) -> \(error!)")
+                if RCHttp.loggingEnabled {
+                    print("RCHttp: POST to \(url) -> \(error!)")
+                }
                 failure(error!)
                 return
             }
-            print("RCHttp: POST to \(url) response: \(String(data: data, encoding: .utf8) ?? "")")
+            if RCHttp.loggingEnabled {
+                print("RCHttp: POST to \(url) response: \(String(data: data, encoding: .utf8) ?? "")")
+            }
             success(response, data)
 		})
 		task!.resume()
@@ -95,29 +139,99 @@ public class RCHttp {
 	
     //mark: put data sync and async
     
-    public func put (at path: String, parameters: [String: Any], success: @escaping (HTTPURLResponse, Data) -> Void, failure: @escaping (Error) -> Void) {
+    public func put (at path: String,
+                     parameters: [String: Any]? = nil,
+                     headers: [String: String]? = nil,
+                     success: @escaping (HTTPURLResponse, Data) -> Void,
+                     failure: @escaping (Error) -> Void) {
         
         let fullPath = baseURL.appendingPathComponent(path).absoluteString.removingPercentEncoding!
         let url = URL(string: fullPath)!
-        let jsonData = try! JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
         
         var request = URLRequest(url: url)
         request.httpMethod = "PUT"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = jsonData
         request = authenticate(request)
-        print("RCHttp: PUT to \(url) -> \(request)")
+        
+        if let parameters = parameters {
+            let jsonData = try! JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+            request.httpBody = jsonData
+        }
+        
+        // Set extra headers
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        for (key, value) in headers ?? [:] {
+            request.setValue(value, forHTTPHeaderField: key)
+        }
+        
+        if RCHttp.loggingEnabled {
+            print("RCHttp: PUT to \(url) -> \(request)")
+        }
         
         let session = URLSession(configuration: URLSessionConfiguration.ephemeral)
         task = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) in
             
-            print("RCHttp: PUT to \(url) response: \(String(describing: response))")
+            if RCHttp.loggingEnabled {
+                print("RCHttp: PUT to \(url) response: \(String(describing: response))")
+            }
             guard let response = response as? HTTPURLResponse, let data = data, error == nil else {
-                print("RCHttp: PUT to \(url) -> \(error!)")
+                if RCHttp.loggingEnabled {
+                    print("RCHttp: PUT to \(url) -> \(error!)")
+                }
                 failure(error!)
                 return
             }
-            print("RCHttp: PUT to \(url) response: \(String(data: data, encoding: .utf8) ?? "")")
+            if RCHttp.loggingEnabled {
+                print("RCHttp: PUT to \(url) response: \(String(data: data, encoding: .utf8) ?? "")")
+            }
+            success(response, data)
+        })
+        task!.resume()
+    }
+    
+    public func delete (at path: String,
+                        parameters: [String: Any]? = nil,
+                        headers: [String: String]? = nil,
+                        success: @escaping (HTTPURLResponse, Data) -> Void,
+                        failure: @escaping (Error) -> Void) {
+        
+        let fullPath = baseURL.appendingPathComponent(path).absoluteString.removingPercentEncoding!
+        let url = URL(string: fullPath)!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        request = authenticate(request)
+        
+        if let parameters = parameters {
+            let jsonData = try! JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+            request.httpBody = jsonData
+        }
+        
+        // Set extra headers
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        for (key, value) in headers ?? [:] {
+            request.setValue(value, forHTTPHeaderField: key)
+        }
+        
+        if RCHttp.loggingEnabled {
+            print("RCHttp: DELETE to \(url) -> \(request)")
+        }
+        
+        let session = URLSession(configuration: URLSessionConfiguration.ephemeral)
+        task = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) in
+            
+            if RCHttp.loggingEnabled {
+                print("RCHttp: DELETE to \(url) response: \(String(describing: response))")
+            }
+            guard let response = response as? HTTPURLResponse, let data = data, error == nil else {
+                if RCHttp.loggingEnabled {
+                    print("RCHttp: DELETE to \(url) -> \(error!)")
+                }
+                failure(error!)
+                return
+            }
+            if RCHttp.loggingEnabled {
+                print("RCHttp: DELETE to \(url) response: \(String(data: data, encoding: .utf8) ?? "")")
+            }
             success(response, data)
         })
         task!.resume()
